a756648f7713249419d91f18c03a01612c93372f
Rainers <rgrigors14@gmail.com>
2023-09-21 15:41:41 +0300

    nepareizs

--- 1uzdevums.py
+++ 1uzdevums.py
@@ -1,1 +1,5 @@
-
+def linolejs(kvadrātmetri,):
+    linoleja_cena_kvadrātmetrā= 3,40
+    return linoleja_cena_kvadrātmetrā*kvadrātmetri
+kvadrāti=200
+print(linulejs,"{}kvadrātmetri linolejs maksās {},EUR")
