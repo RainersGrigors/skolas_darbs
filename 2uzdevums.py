8ff676190b653cce96ec03dd898b9e1ea3b09309
Rainers <rgrigors14@gmail.com>
2023-09-28 14:38:57 +0300

    pirmais

--- 2uzdevums.py
+++ 2uzdevums.py
@@ -1,4 +1,8 @@
 import random
 import math
-def nedēļas_horskops(1teikums,2teikums,3teikums 4teikums)
-apgalvojums
+x= ["Ja tev padomā kāda neparasta ideja","Lai tiktu pie kārotā"," Ja tavs uzdevums būs saistīts ar pierunāšanu vai sarunāšanu"]
+y = ["ķeries pie tās īstenošanas jau šodien","proti novērtēt pretimnākšanu"]
+z =["veiksmīgas var izrādīties pārrunas ar partneriem","neesi skops", "Lielus ieguvumus šodien neplāno"," veltīgs var izrādīties mēģinājums trāpīt desmitniekā ar pirmo šāvienu","esi gatavs veltīt kādu laika sprīdi sagatavošanās darbiem","Grūti tev klāsies"]
+k =["ar pienākumiem tiksi galā bez grūtībām.", "Šodien vairāk paļaujies uz prātu, nekā roku veiklību.","Tava pārliecinošā nostāja un runas veids, iespējams, palīdzēs iegūt jaunus domubiedrus."]
+print(random.randrange[z])
+
