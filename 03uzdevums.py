b35acd34f703926fd10c1b0aa66f31c8a2dd478e
Rainers <rgrigors14@gmail.com>
2023-10-04 09:10:28 +0300

    pirmais

---
+++ 03uzdevums.py
@@ -0,0 +1,13 @@
+def convert(c,y):
+    x=int(input("Uzrkasti skaitli, ja Tu jūties labi(1), ja slikt(2)"))
+    if x == 1:
+        print(c +":)" )
+    elif x== 2:
+        print(y+":(")
+    else:
+        print("mēs jūs nesaprātām")
+convert(c="čau", y="atā")
+   
+        
+        
+        
