473101e791b81148dd641b176ed0e1592698212c
Rainers <rgrigors14@gmail.com>
2023-10-05 13:57:20 +0300

    Update p1_uzdevums.py

--- p1_uzdevums.py
+++ p1_uzdevums.py
@@ -1,1 +1,9 @@
-cvv 
+x = int(input("Ievadiet vārdu uzvārdu"))
+y =int(input("Ievadiet cik stundas jūs esat nostrādājis"))
+z =int(input("Ievadiet savu stundas likmi"))
+def alga(A):
+    if(y > 40):
+        stundas_likme = y / 100 * 10 + y
+    else:
+        stundas_likme=y
+    
