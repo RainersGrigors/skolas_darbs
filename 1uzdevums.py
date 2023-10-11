f7f50d86d24d6a48d11f9527e9790590211c9c4f
Rainers <rgrigors14@gmail.com>
2023-10-11 08:20:48 +0300

    īstais

--- 1uzdevums.py
+++ 1uzdevums.py
@@ -1,10 +1,11 @@
 x = int(input("ievadiet telpas platumu"))
 y = int(input("ievadiet telpas garumu"))
-def linolejs_izmaksas(linoleja_cena_kvadrātmetrā):
+def linolejs_izmaksas(x,y):
+    linoleja_cena_kvadrātmetrā =3.45
     telpas_izmēri = x*y
     return linoleja_cena_kvadrātmetrā*telpas_izmēri
-
-print(linolejs_izmaksas(3,45))
+a = linolejs_izmaksas(x,y)
+print(a)
     
     
 
